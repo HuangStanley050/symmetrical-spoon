204bbfd0e252c5943d297e4ba84b1d2e057c0e31
Stanley Huang <infamous_godhand@yahoo.com>
2021-06-23 07:14:39 +1000

    added dummy component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Greeting from "./components/Greeting";
+import "./App.css";
 
 function App() {
   return (
